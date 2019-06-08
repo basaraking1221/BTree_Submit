8db20e9dd2fc6594eff2900d29caafe6eb0dc8e5
basaraking1221 <48128177+basaraking1221@users.noreply.github.com>
2019-06-08 10:43:07 +0800

    Update BTree.hpp

--- BTree.hpp
+++ BTree.hpp
@@ -1,7 +1,7 @@
-#include "utility.h"
+#include "utility.hpp"
 #include <functional>
 #include <cstddef>
-#include "exception.h"
+#include "exception.hpp"
 #include <fstream>
 namespace sjtu {
     template <class Key, class Value, class Compare = std::less<Key> >
